b7b466cea88f84336b2612908e41e94112b2c4af
Priyanshu-Singhz <ps4151722@gmail.com>
2022-12-19 16:36:58 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000 ;
+const app = express();
+const DB = "mongodb+srv://nodejs_auth:1977Priya#@cluster0.bbg6azj.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+mongoose.connect(DB).then(() => {
+    console.log("Connection successful");
+}).catch((e)=>{
+    console.log("e");
+});
+
+app.use(express.json());
+
+
+app.listen(PORT,"0.0.0.0", () => {
+    console.log('listening on port ${PORT}');
+});
