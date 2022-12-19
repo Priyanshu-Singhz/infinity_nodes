c06c40821e1b8af911b025efd9868772ec2e26a5
Priyanshu-Singhz <ps4151722@gmail.com>
2022-12-19 16:53:04 +0530

    ds

--- server/index.js
+++ server/index.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000 ;
+const PORT = process.env.PORT || 3000;
 const app = express();
-const DB = "mongodb+srv://nodejs_auth:1977Priya#@cluster0.bbg6azj.mongodb.net/?retryWrites=true&w=majority";
-
-mongoose.connect(DB).then(() => {
-    console.log("Connection successful")
-}).catch((e)=>{
-    console.log("e")
-});
-
 app.use(express.json());
 
+const DB =
+  "mongodb+srv://nodejs_auth:1977Priya#@cluster0.bbg6azj.mongodb.net/?retryWrites=true&w=majority";
 
-app.listen(PORT,"0.0.0.0", () => {
-    console.log('Connected at port ${PORT}');
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection successful");
+  })
+  .catch((e) => {
+    console.log("e");
+  });
+
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Connected at port ${PORT}");
 });
