e668c8189ee8028e685ce327af3357d9480258f0
Priyanshu-Singhz <ps4151722@gmail.com>
2022-12-23 22:28:00 +0530

    bhbh

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ mongoose.set('strictQuery', true);
 
 const DB =
   "mongodb+srv://nodejs_auth:test12@cluster0.bbg6azj.mongodb.net/?retryWrites=true&w=majority";
-
+  
 mongoose
   .connect(DB)
   .then(() => {
